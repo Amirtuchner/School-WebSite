3c566ad35eb95c6a8d812646634ac1aca73290fc
Amirtuchner <amir.tuchner@gmail.com>
2025-11-23 20:46:05 +0200

    Update Program.cs
    update

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-   options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseInMemoryDatabase("SchoolDemo"));
+
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
